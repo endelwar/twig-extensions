b96e910f80352dd8c3421467c8294c9e416767a7
Manuel Dalla Lana <endelwar@aregar.it>
2013-10-18 17:43:15 +0200

    more PHPdoc and phpcs compliance

--- src/Endelwar/Twig/ImageExtension.php
+++ src/Endelwar/Twig/ImageExtension.php
@@ -1,15 +1,47 @@
 <?php
 
-class Twig_Extension_Image extends Twig_Extension
+namespace Endelwar\Twig;
+
+/**
+ * Class Twig_Extension_Image
+ *
+ * @package Endelwar\Twig
+ * @author  Manuel Dalla Lana <endelwar@aregar.it>
+ * @license MIT http://opensource.org/licenses/MIT
+ * @link    https://github.com/endelwar/twig-extensions
+ *
+ */
+class ImageExtension extends Twig_Extension
 {
 
     private $_cache_dir;
 
+    /**
+     * {@inheritdoc}
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'image';
+    }
+
+    /**
+     * Constructor
+     * Inject path to cache directory
+     *
+     * @param string $cache_dir cache directory
+     */
     public function __construct($cache_dir)
     {
         $this->_cache_dir = $cache_dir;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     */
     public function getFunctions()
     {
         return array(
@@ -18,19 +50,29 @@ class Twig_Extension_Image extends Twig_Extension
         );
     }
 
+    /**
+     * Open a cached image
+     *
+     * @param string $path image path
+     *
+     * @return object
+     */
     public function image($path)
     {
-        return $this->open($path);
+        return $this->_open($path);
     }
 
+    /**
+     * Create a new image
+     *
+     * @param int $width  image width
+     * @param int $height image height
+     *
+     * @return object
+     */
     public function newImage($width, $height)
     {
-        return $this->create($width, $height);
-    }
-
-    public function getName()
-    {
-        return 'image';
+        return $this->_create($width, $height);
     }
 
     /**
@@ -40,34 +82,39 @@ class Twig_Extension_Image extends Twig_Extension
      *
      * @return object a manipulable image instance
      */
-    private function open($file)
+    private function _open($file)
     {
-        return $this->createInstance($file);
+        return $this->_createInstance($file);
     }
 
     /**
      * Get a new image
      *
-     * @param $w the width
-     * @param $h the height
+     * @param int $w image width
+     * @param int $h image height
      *
      * @return object a manipulable image instance
      */
-    private function create($w, $h)
+    private function _create($w, $h)
     {
-        return $this->createInstance(null, $w, $h);
+        return $this->_createInstance(null, $w, $h);
     }
 
     /**
      * Creates an instance defining the cache directory
+     *
+     * @param string $file file to be handled
+     * @param int    $w    image width
+     * @param int    $h    imege height
+     *
+     * @return ImageHandler
      */
-    private function createInstance($file, $w = null, $h = null)
+    private function _createInstance($file, $w = null, $h = null)
     {
         //$asset = $this->container->get('templating.helper.assets');
         //$full_file_path = '../../..' . $file;
         $full_file_path = substr($file, 1);
         //echo $full_file_path;
-
 
         $image = new ImageHandler($full_file_path, $w, $h);
 
